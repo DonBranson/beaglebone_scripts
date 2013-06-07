80ee33a7a079133bc19a37b2bcef9e4226e8efb9
Don Branson <don.branson@savvis.com>
2013-06-07 10:02:10 -0500

    Cosmetic changes.

--- freezer/freezer.sh
+++ freezer/freezer.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # http://www.armhf.com/index.php/using-beaglebone-black-gpios/
-# pin 9:11, gpio0[30] - 0 + 30 = 30
+# pin 9:11, gpio0[30] -> 0 + 30 = 30
 echo 30 > /sys/class/gpio/export
 echo in > /sys/class/gpio/gpio30/direction
-cat /sys/class/gpio/gpio30/value
 
 # 1=switch open; 0=switch closed
 while [ 1 ] ; do cat /sys/class/gpio/gpio30/value ; sleep .5 ; done
